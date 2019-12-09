e47a9b40011e20db59f4757f0d2d4da4b24e7e23
Dave86 <davidochando1986@gmail.com>
2019-12-09 18:14:33 +0100

    Organizando Estilos.

---
+++ src/components/home/home.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import './home.css';
+
+
+
+class Home extends Component {
+	
+    render() {
+        return (
+            <div className="home">
+					
+                    <header >
+						<img src="https://i.gyazo.com/153ce44e8749482c581daac9f42f9c04.png" alt="logo"/>
+			    		
+			   		</header>
+					
+					
+			    	<div className="main">
+			    		
+						<div className="card">
+							
+							<div className="img">
+								<img src="https://www.ikea.com/pt/en/images/products/stefan-chair__0727320_PE735593_S5.JPG" alt="product image"/>
+							</div>
+							
+							<div className="body">
+								
+								<div className="header">
+									<h1 className="title">Silla normal y corriente</h1>
+									<h1 className="price">999.99€</h1>
+								</div>
+								
+								<div className="description">
+									Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum corporis, ipsa laborum nisi blanditiis repellat vitae reiciendis voluptates quaerat! Eveniet excepturi aspernatur doloremque inventore dignissimos nemo iure cupiditate et esse?
+								</div>
+								
+							</div>
+							
+						</div>
+						
+						
+			   	 	</div>
+				
+			</div>
+			
+        )
+    }
+	
+}
+
+export default Home;
