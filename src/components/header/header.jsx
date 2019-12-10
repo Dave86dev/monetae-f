85e21d71bad14bc50e886837f895b939a0424814
Icaruk <adri-rk@hotmail.com>
2019-12-10 16:45:55 +0100

    Header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,37 +1,35 @@
-
-import React, {Fragment} from "react";
+import React, { Fragment } from "react";
 import "./header.css";
 
-
 class Header extends React.Component {
-	
-	constructor () {
+	constructor() {
 		super();
-		
-	};
+	}
 	
 	render() {
 		return (
 			<header>
-				
 				<div className="logo">
-					<img src="https://trello-attachments.s3.amazonaws.com/5de522b655e9ad63df7441fb/5def57a617949c786fc8ec01/261e294ef093b2db52c2bf6d093c2bd1/logoMonetae_3.png" alt="logo" />
+					<img
+						src="https://trello-attachments.s3.amazonaws.com/5de522b655e9ad63df7441fb/5def57a617949c786fc8ec01/261e294ef093b2db52c2bf6d093c2bd1/logoMonetae_3.png"
+						alt="logo"
+					/>
 				</div>
 				
 				<div className="search">
-					
+					<input type="text" placeholder="Búsqueda" />
+					<div className="backgroundIcon">
+					<i class="material-icons">search</i>
+					</div>
 				</div>
 				
 				<div className="nav">
-					
+					<a href="#">Login</a>
+					<a href="#">Register</a>
 				</div>
-				
 			</header>
 		);
-	};
-	
-	
-};
-
+	}
+}
 
 export default Header;
