37c701b3b9ac039593352e0e8641948d3816cb82
Icaruk <adri-rk@hotmail.com>
2019-12-10 18:32:58 +0100

    Logo icono fixeado (se deformaba)

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -12,7 +12,7 @@ class Header extends React.Component {
 		return (
 			<header>
 				<div className="logo">
-					<NavLink exact activeClassName="active" to="/">
+					<NavLink exact to="/">
 						<img
 							src="https://trello-attachments.s3.amazonaws.com/5de522b655e9ad63df7441fb/5def57a617949c786fc8ec01/261e294ef093b2db52c2bf6d093c2bd1/logoMonetae_3.png"
 							alt="logo"
