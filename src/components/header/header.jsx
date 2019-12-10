a80050787ed42856c035c1ba4a01dc0294c06d6b
Icaruk <adri-rk@hotmail.com>
2019-12-10 15:25:25 +0100

    Empezando header

---
+++ src/components/header/header.jsx
@@ -0,0 +1,36 @@
+
+import React, {Fragment} from "react";
+
+
+class Header extends React.Component {
+	
+	constructor () {
+		super();
+		
+	};
+	
+	render() {
+		return (
+			<header>
+				
+				<div className="logo">
+					<img src="https://trello-attachments.s3.amazonaws.com/5de522b655e9ad63df7441fb/5def57a617949c786fc8ec01/261e294ef093b2db52c2bf6d093c2bd1/logoMonetae_3.png" alt="logo" />
+				</div>
+				
+				<div className="search">
+					
+				</div>
+				
+				<div className="nav">
+					
+				</div>
+				
+			</header>
+		);
+	};
+	
+	
+};
+
+
+export default Header;
