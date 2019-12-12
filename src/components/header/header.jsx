77d568ac56a03114328274a9dc57425722afd7a6
Icaruk <adri-rk@hotmail.com>
2019-12-12 18:24:59 +0100

    Header con botones del nav con if logged

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,5 +1,9 @@
-import React from "react";
+
+import React, {Fragment} from "react";
 import { NavLink } from "react-router-dom";
+
+import {session} from "../../utils/session";
+
 
 import "./header.css";
 
@@ -18,12 +22,12 @@ class Header extends React.Component {
 							src="https://trello-attachments.s3.amazonaws.com/5de522b655e9ad63df7441fb/5def57a617949c786fc8ec01/261e294ef093b2db52c2bf6d093c2bd1/logoMonetae_3.png"
 							alt="logo"
 						/>
-					</NavLink>					
+					</NavLink>
 					
 				</div>
-
-
-
+				
+				
+				
 				<div className="search">
 					<input type="text" placeholder="Búsqueda" />
 					<div className="backgroundIcon">
@@ -32,21 +36,70 @@ class Header extends React.Component {
 				</div>
 				
 				<div className="nav">
-					<button>
-						<NavLink exact activeClassName="active" to="/login">
-							Acceder
-						</NavLink>
-					</button>
-					<button>
-						<NavLink exact activeClassName="active" to="/register">
-							Registrarse
-						</NavLink>
-					</button>
+					<BotonesHeader />
 				</div>
 				
 			</header>
 		);
 	}
 }
+
+
+
+function BotonesHeader() {
+	
+	if (session.get()) { // si estoy logeado...
+		
+		return (
+			
+			<Fragment>
+				
+				<button>
+					<NavLink exact to="/profile">
+						Perfil
+					</NavLink>
+				</button>
+				<button>
+					<NavLink exact to="/storage">
+						Mi almacén
+					</NavLink>
+				</button>
+				<button>
+					<NavLink exact to="/cart">
+						Cesta
+					</NavLink>
+				</button>
+				
+			</Fragment>
+					
+		)
+		
+		
+	} else {
+		
+		return (
+			
+			<Fragment>
+				
+				<button>
+					<NavLink exact to="/login">
+						Acceder
+					</NavLink>
+				</button>
+				<button>
+					<NavLink exact to="/register">
+						Registrarse
+					</NavLink>
+				</button>
+				
+			</Fragment>
+					
+		)		
+		
+	};
+	
+}
+
+
 
 export default Header;
