b24cfc6baffdd0aea78027e9cae73bd2374d3ac2
Dave86 <davidochando1986@gmail.com>
2019-12-12 12:16:36 +0100

    Vista register ok.

--- src/components/register/register.jsx
+++ src/components/register/register.jsx
@@ -13,30 +13,36 @@ class Register extends React.Component {
 			<div className="registerMain">
 
 				<div className="registerCard">
-					<h2>Account</h2>
+					<h2>Cuenta</h2>
 					<div className="registerCardInfoA">
-                        <input className="inputRegister" type="text" placeholder="username"></input>
+                        <input className="inputRegister" type="text" placeholder="nombre"></input>
                         <input className="inputRegister" type="text" placeholder="email"></input>
                         <input className="inputRegister" type="text" placeholder="password"></input>
                         <input className="inputRegister" type="text" placeholder="password"></input>
-                        <input className="inputRegister" type="text" placeholder="Secret Question"></input>
-                        <input className="inputRegister" type="text" placeholder="Secret Answer"></input>
+                        <input className="inputRegister" type="text" placeholder="Pregunta secreta"></input>
+                        <input className="inputRegister" type="text" placeholder="Respuesta secreta"></input>
                     </div>
                     <h2>Personal</h2>
                     <div className="registerCardInfoB">
-                        <input className="inputRegister" type="text" placeholder="Address"></input>
-                        <input className="inputRegister" type="text" placeholder="Phone"></input>
-                        <input className="inputRegister" type="text" placeholder="City"></input>
-                        <input className="inputRegister" type="text" placeholder="Country"></input>
-                    </div>
-                    <h2>Billing</h2>
+                        <input className="inputRegister" type="text" placeholder="Dirección"></input>
+                        <input className="inputRegister" type="text" placeholder="Teléfono"></input>
+                        <input className="inputRegister" type="text" placeholder="Ciudad"></input>
+                        <input className="inputRegister" type="text" placeholder="País"></input>
+                        <select className="registerDropdown">
+						    	<option value="1">Cliente</option>
+						    	<option value="2">Vendedor</option>
+						</select>
+					</div>
+                    <h2>Facturación</h2>
                     <div className="registerCardInfoB">
+                        <input className="inputRegister" type="text" placeholder="Número tarjeta"></input>
+                        <input className="inputRegister" type="text" placeholder="Nombre propietario"></input>
+                        <input className="inputRegister" type="text" placeholder="Mes caducidad"></input>
+                        <input className="inputRegister" type="text" placeholder="Año caducidad"></input>
                         <input className="inputRegister" type="text" placeholder="Paypal"></input>
-                        <input className="inputRegister" type="text" placeholder="Card Number"></input>
-                        <input className="inputRegister" type="text" placeholder="Expire Date"></input>
-                        <input className="inputRegister" type="text" placeholder="Card Owner"></input>
                     </div>
-						
+					{/* <h3>Warning placeholder text</h3> */}
+                    <button>Registrar</button>
 				</div>
 
 			</div>
