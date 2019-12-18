3f89ef16f63612b8b0fc93f08f0006af1c83470e
Dave86 <davidochando1986@gmail.com>
2019-12-18 11:50:17 +0100

    details product features

--- src/components/productDetail/productDetail.jsx
+++ src/components/productDetail/productDetail.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import "./productDetail.scss";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
+import Slider from "../slider/slider";
 
 
 class ProductDetail extends React.Component {
@@ -37,73 +38,78 @@ class ProductDetail extends React.Component {
 	render() {
 		
 		return (
-			<div className="productDetailMain">
-				
-				<div className="images">
-					<div className="bigImage br">
-						<img className="img" src={this.props.productData.imageUrl[0]} alt=""/>
-					</div>
 
+			<div className="productDetailMain">
+				<div className="productDetail">
 					
-					<div className="gallery">
-						<img className="miniImg" onClick={ (ev) => this.pulsaFoto(ev, 0) } src={this.props.productData.imageUrl[0]} alt=""/>
-						<img className="miniImg" onClick={ (ev) => this.pulsaFoto(ev, 1) } src={this.props.productData.imageUrl[1]} alt=""/>
-						<img className="miniImg" onClick={ (ev) => this.pulsaFoto(ev, 2) } src={this.props.productData.imageUrl[2]} alt=""/>
-						<img className="miniImg" onClick={ (ev) => this.pulsaFoto(ev, 3) } src={this.props.productData.imageUrl[3]} alt=""/>
-					</div>
-					
-				</div>
-				
-				
-				
-				<div className="detail">
-					<h2 className="title">{this.props.productData.title}</h2>
-					<p className="seller">Vendido por: <a href="/">Pepe</a> </p>
-					<p className="sellerRating">
-						<img className="ratingStar" src="/img/star.png" alt=""/>
-						<img className="ratingStar" src="/img/star.png" alt=""/>
-						<img className="ratingStar" src="/img/star.png" alt=""/>
-						<img className="ratingStar" src="/img/star_half.png" alt=""/>
-						<img className="ratingStar" src="/img/star_disabled.png" alt=""/>
-					</p>
-					<h1 className="price">{this.props.productData.price} €</h1>
-					<p className="description">
-						{this.props.productData.description}
-					</p>
-				</div>
-				
-				
-				
-				<div className="purchase">
-					<h1 className="price">{this.state.quantity * this.props.productData.price } €</h1>
-					
-					<p></p>
-					
-					<div
-						className="cajaDropdown"
-						onChange={ (ev) => this.cambiaDropdown(ev) }
-					>
+					<div className="images">
+						<div className="bigImage br">
+							<img className="img" src={this.props.productData.imageUrl[0]} alt=""/>
+						</div>
+			
 						
-						<p>Cantidad: </p>
-						<select>
-							<option value="1">1</option>
-							<option value="2">2</option>
-							<option value="3">3</option>
-							<option value="4">4</option>
-							<option value="5">5</option>
-							<option value="6">6</option>
-							<option value="7">7</option>
-							<option value="8">8</option>
-							<option value="9">9</option>
-							<option value="10">10</option>
-						</select>
+						<div className="gallery">
+							<img className="miniImg" onClick={ (ev) => this.pulsaFoto(ev, 0) } src={this.props.productData.imageUrl[0]} alt=""/>
+							<img className="miniImg" onClick={ (ev) => this.pulsaFoto(ev, 1) } src={this.props.productData.imageUrl[1]} alt=""/>
+							<img className="miniImg" onClick={ (ev) => this.pulsaFoto(ev, 2) } src={this.props.productData.imageUrl[2]} alt=""/>
+							<img className="miniImg" onClick={ (ev) => this.pulsaFoto(ev, 3) } src={this.props.productData.imageUrl[3]} alt=""/>
+						</div>
 						
 					</div>
 					
-					<button className="purchaseButton">Añadir al carrito</button>
-					<button className="purchaseButton">Comprar</button>
+					
+					
+					<div className="detail">
+						<h2 className="title">{this.props.productData.title}</h2>
+						<p className="seller">Vendido por: <a href="/">Pepe</a> </p>
+						<p className="sellerRating">
+							<img className="ratingStar" src="/img/star.png" alt=""/>
+							<img className="ratingStar" src="/img/star.png" alt=""/>
+							<img className="ratingStar" src="/img/star.png" alt=""/>
+							<img className="ratingStar" src="/img/star_half.png" alt=""/>
+							<img className="ratingStar" src="/img/star_disabled.png" alt=""/>
+						</p>
+						<h1 className="price">{this.props.productData.price} €</h1>
+						<p className="description">
+							{this.props.productData.description}
+						</p>
+					</div>
+					
+					
+					
+					<div className="purchase">
+						<h1 className="price">{this.state.quantity * this.props.productData.price } €</h1>
+						
+						<p></p>
+						
+						<div
+							className="cajaDropdown"
+							onChange={ (ev) => this.cambiaDropdown(ev) }
+						>
+							
+							<p>Cantidad: </p>
+							<select>
+								<option value="1">1</option>
+								<option value="2">2</option>
+								<option value="3">3</option>
+								<option value="4">4</option>
+								<option value="5">5</option>
+								<option value="6">6</option>
+								<option value="7">7</option>
+								<option value="8">8</option>
+								<option value="9">9</option>
+								<option value="10">10</option>
+							</select>
+							
+						</div>
+						
+						<button className="purchaseButton">Añadir al carrito</button>
+						<button className="purchaseButton">Comprar</button>
+					</div>
 				</div>
-				
+
+				<Slider url="/product/best?limit=10" />
+
 			</div>
 		);
 	};
